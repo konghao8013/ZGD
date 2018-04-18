6a5b9f75f3640d9ff081474a20e44f7ea9ce4d65
lijian <197409335@qq.com>
2018-04-18 16:09:08 +0800

    13

--- ZGD.Web/Controllers/GalleryController.cs
+++ ZGD.Web/Controllers/GalleryController.cs
@@ -11,7 +11,7 @@ namespace ZGD.Web.Controllers
     public class GalleryController : BaseController
     {
         /// <summary>
-        /// 新闻首页
+        /// 图册首页
         /// </summary>
         /// <param name="page"></param>
         /// <returns></returns>
@@ -23,15 +23,15 @@ namespace ZGD.Web.Controllers
                 key = key.FunStr();
                 url += "-" + key;
                 key = HttpUtility.UrlDecode(key);
-                where += " and (n.Title like '%" + key + "%' or n.Keyword like '%" + key + "%' or n.Tags like '%" + key + "%')";
+                where += " and (p.Title like '%" + key + "%' or p.Keyword like '%" + key + "%' or p.Tags like '%" + key + "%')";
             }
 
             if (t > 0)
             {
-                where += " and nc.ClassId=" + t;
+                where += " and p.ClassId=" + t;
                 url += "-" + t;
             }
-            var result = new ZGD.BLL.NewsInfo().GetList(page, 15, where, " n.IsTop desc,n.PubTime desc", url, "/articlepage/", out pager);
+            var result = new ZGD.BLL.Project().GetList(page, 15, where, " p.IsTop desc,p.PubTime desc", url, "/gallerypage/", out pager);
             DataSet ds = new ZGD.BLL.Channel().GetList(0, " ParentId=1 and IsDelete=0", " Id asc");
             ViewBag.ClassType = ds != null && ds.Tables[0].Rows.Count > 0 ? ds.Tables[0] : null;
             var cModel = new ZGD.BLL.Channel().GetModelById(t);
@@ -40,51 +40,29 @@ namespace ZGD.Web.Controllers
             ViewBag.SearchKey = key;
             ViewBag.Type = cModel != null ? cModel.Title : key;
             ViewBag.TypeID = t;
-            //广告
-            DataSet ds_ad = new ZGD.BLL.Banner().GetList(0, " aType=2 and IsLock=0", " Sort asc");
-            ViewBag.AdList = ds_ad != null ? ds_ad.Tables[0] : null;
+
             return View(result);
         }
 
         /// <summary>
-        /// 新闻详细
+        /// 图册详细
         /// </summary>
         /// <param name="id"></param>
         /// <returns></returns>
-        public ActionResult Details(int cid, int id)
+        public ActionResult Details(int id)
         {
-            ZGD.BLL.NewsInfo bll = new BLL.NewsInfo();
-            ZGD.Model.NewsInfo news = bll.GetModel(id);
+            ZGD.BLL.Project bll = new BLL.Project();
+            ZGD.BLL.ProjectImg iBll = new BLL.ProjectImg();
+            ZGD.Model.Project model = bll.GetModel(id);
             //点击率
             bll.UpdateField(id, " Click=Click+1");
 
-            Model.Channel cType = new BLL.Channel().GetModelById(cid);
-            news.ClassName = cType == null ? "" : cType.Title;
-            ViewBag.TypeID = cid.ToString();
+            Model.Channel cType = new BLL.Channel().GetModelById(model.ClassId);
+            model.TypeName = cType == null ? "" : cType.Title;
 
-            return View(news);
-        }
+            ViewBag.Pics = iBll.GetList(0, " pID=" + model.Id, " piID asc");
 
-        /// <summary>
-        /// 判断是否为专题
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
-        public static bool IsZT(int id)
-        {
-            if (id <= 0)
-            {
-                return false;
-            }
-            Model.Channel cType = new BLL.Channel().GetModelById(id);
-            if (cType != null)
-            {
-                if (cType.ParentId == 21)
-                {
-                    return true;
-                }
-            }
-            return false;
+            return View(model);
         }
     }
 }
