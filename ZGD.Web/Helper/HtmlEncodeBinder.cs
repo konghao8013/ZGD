79c24a0a39d882f8f1ed2c5bee22a59031676e18
konghao <konghao8013@163.com>
2023-11-08 19:17:11 +0800

    增加Html编码展示封装

---
+++ ZGD.Web/Helper/HtmlEncodeBinder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+
+public class HtmlEncodeBinder
+{
+    public static string HtmlEncodeEval(object dataItem, string expression)
+    {
+        object value = DataBinder.Eval(dataItem, expression);
+        if (value != null)
+        {
+            return HttpUtility.HtmlEncode(value.ToString());
+        }
+        return string.Empty;
+    }
+}
